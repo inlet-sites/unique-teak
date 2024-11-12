621c985530e2aca7568e0f7124b1441ebf19e490
Lee Morgan <lee@leemorgan.dev>
2024-11-12 16:24:28 -0500

    Initial commit and create header.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import "../global.css";
+    import Header from "./Header.svelte";
+</script>
+
+<Header/>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,49 @@
+<script>
+    import background from "$lib/background.webp";
+    import headboard from "$lib/headboard.png";
+</script>
+
+<header>
+    <img class="headerBackground" src={background} alt="Teak Wood pattern">
+    <h1>Unique Teak Creations</h1>
+    <img class="headerImage" src={headboard} alt="Teak headboard">
+    <div>
+        <h2>300 - 500 year old, Grade A, Teak Furniture</h2>
+        <h2>Beautifully Handcrafted by Skilled Artisans</h2>
+    </div>
+</header>
+
+<div class="content">
+    
+</div>
+
+<style>
+    header{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        position: relative;
+        height: 75vh;
+        width: 100vw;
+        color: rgb(225, 225, 225);
+    }
+
+    .headerBackground{
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        width: 100%;
+        height: 100%;
+        filter: brightness(50%);
+    }
+
+    header h1{
+        font-size: 45px;
+    }
+
+    .headerImage{
+        height: 75%;
+    }
+</style>
