a7bc930a15252201cc8cc1f031755458d845b4a5
Lee Morgan <lee@leemorgan.dev>
2024-11-14 17:57:24 -0500

    Create temporary gallery and product pages.

---
+++ src/routes/gallery/+page.svelte
@@ -0,0 +1,25 @@
+<div class="container">
+    <a href="/">Home</a>
+    <h1>Coming Soon!</h1>
+</div>
+
+<style>
+    .container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        width: 100vw;
+    }
+
+    h1{
+        font-size: 38px;
+    }
+
+    a{
+        position: fixed;
+        top: 35px;
+        left: 35px;
+        font-size: 28px;
+    }
+</style>
