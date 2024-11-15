bf53f215e0b7ae20723102b866bdcc233b779411
Lee Morgan <lee@leemorgan.dev>
2024-11-15 11:50:03 -0500

    Display tags for all products.

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,25 +1,109 @@
+<script>
+    import "../../global.css";
+
+    let products = $state([]);
+    let tags = $state({});
+    let activeTag = $state("all");
+
+    const sortByTag = (data)=>{
+        const tags = {};
+        tags["all"] = [];
+        for(let i = 0; i < data.length; i++){
+            tags["all"].push(data[i]);
+            for(let j = 0; j < data[i].tags.length; j++){
+                let tag = data[i].tags[j].toLowerCase();
+                if(tags[tag]){
+                    tags[tag].push(data[i]);
+                }else{
+                    tags[tag] = [data[i]];
+                }
+            }
+        }
+        return tags;
+    }
+
+    const searchTags = (tag)=>{
+        activeTag = tag;
+        products = tags[tag];
+    }
+
+    fetch("https://api.inlet.shop/product/vendor/672cd710c3fa18c32ec1c18c", {
+        method: "get",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(r=>r.json())
+        .then((response)=>{
+            if(response.error){
+                console.error(error);
+            }else{
+                tags = sortByTag(response);
+                products = tags.all;
+            }
+        })
+        .catch((err)=>{
+            console.error(err);
+        });
+</script>
+
 <div class="container">
-    <a href="/">Home</a>
-    <h1>Coming Soon!</h1>
+    <h1>Unique Teak Creations</h1>
+
+    <div class="tags">
+        {#each Object.keys(tags) as tag}
+            <button
+                class="tag"
+                class:active={tag === activeTag}
+                onclick={()=>{searchTags(tag)}}
+            >{tag.toUpperCase()}</button>
+        {/each}
+    </div>
+
+    <div class="products">
+        {#each products as product}
+            <h2>{product.name}</h2>
+        {/each}
+    </div>
 </div>
 
 <style>
     .container{
         display: flex;
-        justify-content: center;
+        flex-direction: column;
         align-items: center;
-        height: 100vh;
-        width: 100vw;
+        background: rgb(200, 200, 200);
+        min-height: 100vh;
+        padding: 35px;
     }
 
-    h1{
+    .container h1{
         font-size: 38px;
+        margin-bottom: 35px;
     }
 
-    a{
-        position: fixed;
-        top: 35px;
-        left: 35px;
-        font-size: 28px;
+    .tags{
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        width: 75%;
+        margin-bottom: 35px;
+    }
+
+    .tag{
+        background: white;
+        border: none;
+        padding: 5px 10px;
+        cursor: pointer;
+        border: 1px solid black;
+        margin-right: -1px;
+    }
+
+    .tag:hover{
+        background: rgb(235, 235, 235);
+    }
+
+    .tag.active{
+        border-bottom: 2px solid red;
     }
 </style>
